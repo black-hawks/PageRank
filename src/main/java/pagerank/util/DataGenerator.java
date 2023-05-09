eedb43dd9ff1b52fdfccb2209cf316a74463f377
Purvish Shah <purvish14@gmail.com>
2023-05-09 19:28:36 +0100

    Java Doc for DataGenerator

--- src/main/java/pagerank/util/DataGenerator.java
+++ src/main/java/pagerank/util/DataGenerator.java
@@ -6,12 +6,21 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * A utility class for generating data and graph for PageRank algorithm.
+ */
 public class DataGenerator {
   private FileReader fileReader;
 
   private Set<Integer> vertices;
 
-
+  /**
+   * Calculates the number of vertices in the given file.
+   *
+   * @param filename The filename of the file to be read.
+   * @return The number of vertices in the given file.
+   * @throws IOException if the file cannot be read.
+   */
   public int numOfVertices(String filename) throws IOException {
     String[] line;
     vertices = new HashSet<>();
@@ -24,10 +33,23 @@ public class DataGenerator {
     return vertices.size();
   }
 
-  public Set<Integer> getVertices(){
+  /**
+   * Returns the set of vertices generated from the given file.
+   *
+   * @return The set of vertices.
+   */
+  public Set<Integer> getVertices() {
     return vertices;
   }
 
+  /**
+   * Generates a graph from the given file using the specified graph implementation.
+   *
+   * @param graph    The graph implementation to use.
+   * @param filename The filename of the file to be read.
+   * @return The generated graph.
+   * @throws IOException if the file cannot be read.
+   */
   public Graph<Integer> generateGraph(Graph<Integer> graph, String filename) throws IOException {
     String[] line;
     this.fileReader = new FileReader(filename, "\t");
@@ -37,5 +59,4 @@ public class DataGenerator {
     this.fileReader.closeStream();
     return graph;
   }
-
 }

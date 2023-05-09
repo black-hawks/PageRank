3c1b8d88b83366c395ba8a982e2184c2cc33b766
Ravi Raj Pedada <ravirj1810@gmail.com>
2023-05-09 14:56:42 +0530

    Adjacency List Code

--- src/main/java/pagerank/algorithms/CycleCount.java
+++ src/main/java/pagerank/algorithms/CycleCount.java
@@ -36,53 +36,57 @@ public class CycleCount {
     return false;
   }
 
-  public static int isCyclic(Graph<Node> graph) {
+  public static void isCyclic(Graph<Node> graph) {
     LinkedList<Node> visited = new LinkedList();
     LinkedList<Node> recursionStack = new LinkedList();
 
     Set<Node> vertices  = ((AdjacencyListGraph)graph).getAllNodes();
     for(Node node : vertices){
-      if (!visited.contains(node) && isCyclicUtil(graph, graph.getNeighbors(node).get(0), visited, recursionStack)) {
-        count++;
+      if(graph.getNeighbors(node).size() != 0){
+        if (!visited.contains(node) && isCyclicUtil(graph, graph.getNeighbors(node).get(0), visited, recursionStack)) {
+          count++;
+        }
       }
+
     }
-//    for (int i = 0; i < graph.getNumNodes(); i++) {
-//      if (!visited.contains(i) && isCyclicUtil(graph, graph.getNeighbors().get(0), visited, recursionStack)) {
-//        count++;
-//      }
-//    }
-
-    return count;
-  }
-
-
-
-    public static void main(String args[]) {
-      Graph g = new AdjacencyListGraph();
-      Node node1 = new Node(1);
-      Node node2 = new Node(2);
-      Node node3 = new Node(3);
-      Node node4 = new Node(4);
-      Node node5 = new Node(5);
-      Node node6 = new Node(6);
-      Node node7 = new Node(7);
-      Node node8 = new Node(8);
-
-      g.addEdge(node1, node2);
-      g.addEdge(node2, node3);
-      g.addEdge(node3, node1);
-      g.addEdge(node4, node5);
-      g.addEdge(node5, node6);
-      g.addEdge(node6, node7);
-      g.addEdge(node7, node4);
-
-      if (isCyclic(g) > 0) {
+      if (count > 0) {
         System.out.println("Cycle detected");
         System.out.println("Graph contains " + count + " cycles");
       } else {
         System.out.println("Graph doesn't contain cycle");
       }
-    }
+
+//  return count;
+  }
+
+
+
+//    public static void main(String args[]) {
+//      Graph g = new AdjacencyListGraph();
+//      Node node1 = new Node(1);
+//      Node node2 = new Node(2);
+//      Node node3 = new Node(3);
+//      Node node4 = new Node(4);
+//      Node node5 = new Node(5);
+//      Node node6 = new Node(6);
+//      Node node7 = new Node(7);
+//      Node node8 = new Node(8);
+//
+//      g.addEdge(node1, node2);
+//      g.addEdge(node2, node3);
+//      g.addEdge(node3, node1);
+//      g.addEdge(node4, node5);
+//      g.addEdge(node5, node6);
+//      g.addEdge(node6, node7);
+//      g.addEdge(node7, node4);
+//
+//      if (isCyclic(g) > 0) {
+//        System.out.println("Cycle detected");
+//        System.out.println("Graph contains " + count + " cycles");
+//      } else {
+//        System.out.println("Graph doesn't contain cycle");
+//      }
+//    }
 }
 
 
--- src/main/java/pagerank/datastructure/adjacentList/Main.java
+++ src/main/java/pagerank/datastructure/adjacentList/Main.java
@@ -1,18 +1,23 @@
 package pagerank.datastructure.adjacentList;
 
+import pagerank.algorithms.CycleCount;
 import pagerank.datastructure.Graph;
+import pagerank.util.CsvFileHandler;
 import pagerank.util.DataGenerator;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
         DataGenerator dataGenerator = new DataGenerator();
+
+        long start = System.nanoTime();
         Graph<Node> graph = dataGenerator.generateGraph(new AdjacencyListGraph<>());
+
+        long end = System.nanoTime();
 
 //        for(Map.Entry<Node, List<Node>> list : ((AdjacencyListGraph<Node>) graph).getAdjacencyList().entrySet()){
 //            System.out.println("Vertex : " + list.getKey());
@@ -35,22 +40,26 @@ public class Main {
 //        graph.addEdge(node4, node3);
 //        graph.addEdge(node4, node3);
 
-        System.out.println(graph.getNumNodes());
+        System.out.println("Number of Nodes present in the Graph : " + graph.getNumNodes());
+
+        System.out.println("Time taken to generate the Graph " + (double)(end - start)/ 1_000_000_000.0);
+
 
         // Calculate the PageRank for each node
         PageRank pageRank = new PageRank(graph, graph.getNumNodes());
-        List<Node> pageRankValues = pageRank.calculatePageRank();
-//        Collections.sor
-        int i =0;
-        // Print the PageRank values for each node
-        for(Node node: pageRankValues){
-            System.out.println(node.getId() + " : value " + node.getCurrentRank());
-            i++;
-            if(i>10){
-                break;
-            }
-        }
 
+        start = System.nanoTime();
+        List<Node> pageRankValues = pageRank.calculatePageRank();
+
+        end = System.nanoTime();
+
+        System.out.println("Time taken to calculate  Page Rank " + (double)(end - start)/ 1_000_000_000.0);
+
+        CsvFileHandler.csvWriter("Output.csv", pageRankValues);
+
+        CsvFileHandler.csvReader("Output.csv");
+
+//        CycleCount.isCyclic(graph);
     }
 }
 
